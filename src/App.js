c13400d389483f542c20e2a3fc5adf18c169c1d7
Pedro Alex <peupedroalex@yahoo.com.br>
2022-09-08 22:06:56 -0300

    First flag projects commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const fetchFlags = () => {
+    return axios.get("https://restcountries.com/v3.1/all").then((res) => {
+      console.log(res.data);
+      setData(res.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchFlags();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {data.map((item, key) => {
+          return (
+            <>
+              <img src={item.flags.png}></img>
+              <p key={key}>{item.name.common}</p>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
