de2d8117387899938d456a95e666ec5e325b1b8e
Pedro Alex <peupedroalex@yahoo.com.br>
2022-09-13 23:03:19 -0300

    search input implementation and render refactor

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
 
   const fetchFlags = () => {
     return axios.get("https://restcountries.com/v3.1/all").then((res) => {
@@ -16,17 +17,54 @@ function App() {
     fetchFlags();
   }, []);
 
+  function renderFlags(){
+    if(search !== ''){
+    const names = data.filter(item=>{
+      let name = item.name.common.trim().toLowerCase()
+      if(name.includes(search.trim().toLowerCase())) {
+        return item
+      }
+      else{
+        return ''
+      }
+    })
+    console.log(names)
+    return (
+      <div>
+      {names.map((item, key) => {
+        return !item ? '' :(
+          <>
+            <img alt={`Flag of ${item.name.common}`} src={item.flags.png}></img>
+            <p key={key}>{item.name.common}</p>
+          </>
+        );
+      })}
+      </div>
+    )
+  }else{
+    return(
+      <div>
+      {data.map((item, key) => {
+        return !item ? '' :(
+          <>
+            <img alt={`Flag of ${item.name.common}`} src={item.flags.png}></img>
+            <p key={key}>{item.name.common}</p>
+          </>
+        );
+      })}
+      </div>
+    )
+  }
+  }
+
   return (
     <div>
+      <span>
+            <label htmlFor="searchFlags">Search</label>
+            <input value={search} id="searchFlags" type="text" onChange={e=>setSearch(e.target.value)}></input>
+            </span>
       <div>
-        {data.map((item, key) => {
-          return (
-            <>
-              <img src={item.flags.png}></img>
-              <p key={key}>{item.name.common}</p>
-            </>
-          );
-        })}
+        {renderFlags()}
       </div>
     </div>
   );
